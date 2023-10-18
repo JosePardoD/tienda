f962e1a60a5d327ee94b6b95e0bbeeda325d87fe
JosePardoD <88119233+JosePardoD@users.noreply.github.com>
2023-10-18 08:01:53 -0500

    segundo

--- src/main/java/com/josepardo/tienda/service/ProductoService.java
+++ src/main/java/com/josepardo/tienda/service/ProductoService.java
@@ -1,4 +1,14 @@
 package com.josepardo.tienda.service;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ProductoService {
+
+    private ProductoService productoService;
+
+
 }
